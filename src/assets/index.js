4fe2c4a52230a36661c0c007ea67de29cdd9b7bf
Kunal <kunalpaul673@gmail.com>
2025-03-29 10:34:05 +0530

    First commit

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import education from "./education.png";
+import holycross from "./holycross.png";
+import onboarding from "./onboarding.jpg";
+import oops from "./oops.png"
+export { education, holycross, onboarding, oops };
